fb658dc9ecdc791c1905746a059c64db1defa355
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-05-07 20:57:22 +0600

    solved fourth problem in assignment

--- main.ts
+++ main.ts
@@ -37,5 +37,41 @@
 
     //==============> Problem 3 <==============//
 
+    // Pore korbo
+
+    //==============> Problem 4 <==============//
+
+      class Vehicle {
+      public  make: string;
+      public  year: number;
+
+        constructor(make: string, year: number) {
+            this.make = make;
+            this.year = year;
+        }
+
+        getInfo() {
+            return `Make: ${this.make}, Year: ${this.year}`
+        }
+    }
+    
+
+    class Car extends Vehicle {
+        private model: string;
+
+        constructor(make: string, year: number, model: string) {
+            super(make, year);
+            this.model = model;
+        }
+
+        getModel() {
+            return `Model: ${this.model}`
+        }
+    }
+
+    const myCar = new Car("Toyota", 2020, "Corolla");
+    myCar.getInfo();   // Output: "Make: Toyota, Year: 2020"
+    myCar.getModel();  // Output: "Model: Corolla"
+
 
 }
