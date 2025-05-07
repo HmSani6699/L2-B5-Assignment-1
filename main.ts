1e67a2f7bb4f34b42c264e45ea9b77c4f8890a30
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-05-07 19:57:12 +0600

    Completed Problem 1

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
 {
 
   // ts-node-dev --respawn --transpile-only server.ts
+ 
+    //==============> Problem 1 <==============//
+     function formatString(input: string, toUpper?: boolean) : string {
+      
+        return  toUpper === true ? input.toUpperCase(): toUpper === false ? input.toLowerCase():input.toUpperCase();
+    
+    };
 
+    const result1 = formatString("Hello");          // Output: "HELLO"
+    const result2 =formatString("Hello", true);     // Output: "HELLO"
+    const result3= formatString("Hello", false);    // Output: "hello"
 
 
 
--- p1.ts
+++ p1.ts
@@ -1,8 +1,26 @@
 {
 
-  
+    function formatString(input: string, toUpper?: boolean) : string {
+        // if (toUpper === true) {
+        //     return input.toUpperCase();
+        // } else if (toUpper === false) {
+        //      return input.toLowerCase();
+        // } else {
+        //     return input.toUpperCase();
+        // }
+
+        return  toUpper === true ? input.toUpperCase(): toUpper === false ? input.toLowerCase():input.toUpperCase();
+    
+    };
+
+    const result = formatString("Hello");          // Output: "HELLO"
+    const result1 =formatString("Hello", true);   // Output: "HELLO"
+    const result2= formatString("Hello", false);  // Output: "hello"
+
+    console.log(result2);
+    
 
 
-    console.log('hallo testing ');
+
     
 }
