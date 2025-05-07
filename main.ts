1d0c1aa119128cf2c430d52c86b6e9cc6db67aec
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-05-07 20:14:45 +0600

    Solbed the second problem in assignment 1

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 {
 
-  // ts-node-dev --respawn --transpile-only server.ts
- 
     //==============> Problem 1 <==============//
      function formatString(input: string, toUpper?: boolean) : string {
       
@@ -13,6 +11,31 @@
     const result2 =formatString("Hello", true);     // Output: "HELLO"
     const result3= formatString("Hello", false);    // Output: "hello"
 
+
+
+    //==============> Problem 2 <==============//
+     function filterByRating(items: {
+        title: string;
+        rating: number;
+    }[]): {
+        title: string;
+        rating: number;
+    }[]{
+
+        return items.filter((item) => item.rating >= 4);
+    };
+    
+    const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+    ];
+
+    filterByRating(books); 
+    // Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
+
+
+    //==============> Problem 3 <==============//
 
 
 }
--- p1.ts
+++ p1.ts
@@ -1,26 +1,16 @@
 {
 
-    function formatString(input: string, toUpper?: boolean) : string {
-        // if (toUpper === true) {
-        //     return input.toUpperCase();
-        // } else if (toUpper === false) {
-        //      return input.toLowerCase();
-        // } else {
-        //     return input.toUpperCase();
-        // }
 
-        return  toUpper === true ? input.toUpperCase(): toUpper === false ? input.toLowerCase():input.toUpperCase();
+    // ts-node-dev --respawn --transpile-only server.ts
+
+
+ 
+
     
-    };
-
-    const result = formatString("Hello");          // Output: "HELLO"
-    const result1 =formatString("Hello", true);   // Output: "HELLO"
-    const result2= formatString("Hello", false);  // Output: "hello"
-
-    console.log(result2);
+    const result = ; 
     
-
-
+    console.log(result);
+    
 
     
 }
