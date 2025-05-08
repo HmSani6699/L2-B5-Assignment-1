120627d2520a2a81f80fe2c292ada299d77760a3
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-05-08 10:59:52 +0600

    solved the all problem in assignment

--- main.ts
+++ main.ts
@@ -1,49 +1,27 @@
 {
-
     //==============> Problem 1 <==============//
      function formatString(input: string, toUpper?: boolean) : string {
-      
         return  toUpper === true ? input.toUpperCase(): toUpper === false ? input.toLowerCase():input.toUpperCase();
-    
     };
 
-    const result1 = formatString("Hello");          // Output: "HELLO"
-    const result2 =formatString("Hello", true);     // Output: "HELLO"
-    const result3= formatString("Hello", false);    // Output: "hello"
-
-
-
     //==============> Problem 2 <==============//
-     function filterByRating(items: {
-        title: string;
-        rating: number;
-    }[]): {
+    function filterByRating(items: {title: string;rating: number;}[]): {
         title: string;
         rating: number;
     }[]{
-
         return items.filter((item) => item.rating >= 4);
     };
-    
-    const books = [
-    { title: "Book A", rating: 4.5 },
-    { title: "Book B", rating: 3.2 },
-    { title: "Book C", rating: 5.0 }
-    ];
-
-    filterByRating(books); 
-    // Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
-
 
     //==============> Problem 3 <==============//
-
-    // Pore korbo
+    function concatenateArrays<T>(...arrays: T[][]): T[]{
+        const result = arrays.reduce((prev, current) => [...prev, ...current]);
+        return result;
+    }
 
     //==============> Problem 4 <==============//
-
       class Vehicle {
-      public  make: string;
-      public  year: number;
+      private  make: string;
+      private  year: number;
 
         constructor(make: string, year: number) {
             this.make = make;
@@ -54,7 +32,6 @@
             return `Make: ${this.make}, Year: ${this.year}`
         }
     }
-    
 
     class Car extends Vehicle {
         private model: string;
@@ -69,24 +46,54 @@
         }
     }
 
-    const myCar = new Car("Toyota", 2020, "Corolla");
-    myCar.getInfo();   // Output: "Make: Toyota, Year: 2020"
-    myCar.getModel();  // Output: "Model: Corolla"
-
-
-
-
     //==============> Problem 5 <==============//
-
-      function processValue(value: string | number): number{
+    function processValue(value: string | number): number{
         return typeof value === "string" ? value?.length : value * 2;
     }
 
-    processValue("hello");    // Output: 5
-    processValue(10);         // Output: 20
+    //==============> Problem 6 <==============//
+    interface Product {
+        name: string;
+        price: number;
+    }
+    
+    function getMostExpensiveProduct(products: Product[]): Product | null {
+        const result = products?.reduce((prev, current) => (prev.price > current?.price ? prev : current), products[0]);
+        return result || null
+    }
+    
+    //==============> Problem 7 <==============//
+    enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    
+    function getDayType(day: Day): string{
+        if (Day[day] === "Saturday" || Day[day] === "Sunday") {
+            return "Weekend"
+        } else {
+            return "Weekday"
+        }
+    }
 
-
-
+    //==============> Problem 8 <==============//
+     async function squareAsync(n: number): Promise<number>{
+        return new Promise<number>((resolve, reject) => {
+           if (n && n >0) {
+               setTimeout(() => {
+                   resolve(n*n)
+               },1000)
+           } else {
+               reject('Error: Negative number not allowed')
+           }
+       })
+   }
+    
 
 
 
